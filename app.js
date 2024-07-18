c8365c15f5fae392d9626d60605c29b61150756d
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-18 22:50:46 +0530

    completed  backend file structure and mongodb connection

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+
+const dbConnection =require('./config/db')
+const authRouter = require('./routes/authRoutes')
+
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/',authRouter);
+
+dbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+  });
