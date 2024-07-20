ad921cb9551a3f5537820cb6ffa0ae2d1c05cddb
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-20 20:17:23 +0530

    authrouter completed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const authController = require("../controller/authcontroller")
 
 router.post("/signup", authController.userSignupPost);
+router.post("/login",authController.userLoginPost)
+router.post("/google-signup", authController.googleSignupPost);
+router.post("/google-login", authController.googleLoginPost);
+
 
 
 module.exports = router;
