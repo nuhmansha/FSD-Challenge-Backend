4dcbbce81b94ddb3ae8fcb7feb58aeba39f692cc
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-20 08:02:56 +0530

    created task schema

---
+++ models/task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['todo', 'in-progress', 'done'],
+    default: 'todo',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+}, {
+  timestamps: true,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
