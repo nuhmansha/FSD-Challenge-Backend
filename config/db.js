6c8fd5f9c428e0cee966e704be68e0620631b1fa
nuhmansha <155939879+nuhmansha@users.noreply.github.com>
2024-07-22 23:32:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // 5 seconds timeout
+    });
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Error connecting DB:", error);
   }
 };
 
+connectDB();
+
 module.exports = connectDB;
+
