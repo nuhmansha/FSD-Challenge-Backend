b85a00beaa4bf4fdf8c28a0c2e37376315ae44bc
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-22 13:10:52 +0530

    Add routes for task management with authentication middleware

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -8,5 +8,7 @@ router.get("/tasks", authMiddleware, taskcontroller.getTasks);
 router.post("/addtasks", authMiddleware, taskcontroller.createTask);
 router.put("/tasks/:taskId", authMiddleware, taskcontroller.updateTaskStatus);
 router.delete("/taskdelete/:taskId", authMiddleware, taskcontroller.deleteTask);
+router.put("/taskedit/:taskId", authMiddleware, taskcontroller.updateTask);
+router.get("/tasks/:taskId", authMiddleware, taskcontroller.getTaskById);
 
 module.exports = router;
