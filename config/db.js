cfb80615d269d00937fdd5b8fb060b18ec886869
nuhmansha <155939879+nuhmansha@users.noreply.github.com>
2024-07-23 06:42:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
